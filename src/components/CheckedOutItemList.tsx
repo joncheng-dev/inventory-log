410e9a4c453ecfaf482d70dd337c677da099b088
thebyronc <thebyronc@gmail.com>
2025-09-30 10:35:03 -0700

    fix naming and imports

--- src/components/CheckedOutItemList.tsx
+++ src/components/CheckedOutItemList.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import CheckedOutItem from "./CheckedOutItem";
 
 export default function CheckedOutItemList() {
--- src/components/filter/SearchInput.tsx
+++ src/components/filter/SearchInput.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function SearchInput() {
   return (
     <div className='relative'>
