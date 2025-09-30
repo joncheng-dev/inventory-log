a907045e267d57430a97e9d3ce6e700a24229f3e
Jon Cheng <joncheng.dev@gmail.com>
2025-10-01 02:29:24 +0900

    rearrange component layout for InventoryPage

--- src/pages/InventoryPage.jsx
+++ src/pages/InventoryPage.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import Header from '../components/Header';
-import Filters from '../components/Filters';
+import Filters from '../components/filter/Filters';
 import ItemList from '../components/ItemList';
-import InfoPanel from '../components/InfoPanel';
+import CheckedOutItemList from '../components/CheckedOutItemList';
 
 export default function InventoryPage() {
   return (
     <div className="flex flex-col h-screen w-full">
       <Header />
+      <div className="flex w-full border border-fuchsia-300">
+        <Filters/>
+      </div>
       <div className="flex flex-1 w-full">
-        <div className="w-1/6 border border-lime-200">
-          <Filters />
-        </div>
         <div className="flex-1 border border-cyan-300">
           <ItemList />
         </div>
         <div className="w-1/5 border border-teal-200">
-          <InfoPanel/>
+          <CheckedOutItemList/>
         </div>
       </div>
     </div>
