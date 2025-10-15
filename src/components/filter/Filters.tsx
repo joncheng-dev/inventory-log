2c05a0505405ef381361c6294ee20a87e90cdb9d
Jon Cheng <joncheng.dev@gmail.com>
2025-10-15 17:42:17 +0900

    update Filters.tsx component; reorder props

--- src/components/filter/Filters.tsx
+++ src/components/filter/Filters.tsx
@@ -3,14 +3,14 @@ import TagFilter from "./TagFilter";
 import ViewToggle from "./ViewToggle";
 
 interface FiltersProps {
-  viewMode: 'grid-view' | 'list-view';
-  setViewMode: React.Dispatch<React.SetStateAction<'grid-view' | 'list-view'>>;
   availableFilterTags: Array<string>;
   selectedTags: Array<string>;
   setSelectedTags: React.Dispatch<React.SetStateAction<string[]>>;
+  viewMode: 'grid-view' | 'list-view';
+  setViewMode: React.Dispatch<React.SetStateAction<'grid-view' | 'list-view'>>;
 }
 
-export default function Filters({ viewMode, setViewMode, availableFilterTags, selectedTags, setSelectedTags }: FiltersProps) {
+export default function Filters({ availableFilterTags, selectedTags, setSelectedTags, viewMode, setViewMode }: FiltersProps) {
   return (
     <div className="flex items-center gap-4 px-4 py-3 bg-theme-surface border-b border-theme">
       <SearchInput />
