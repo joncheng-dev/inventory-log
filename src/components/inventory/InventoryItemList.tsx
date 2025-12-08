fe7e2026c8d23c51faa61a3fc975bd719ed7309c
thebyronc <thebyronc@gmail.com>
2025-12-07 23:11:25 -0800

    padding fix to match rest of the site

--- src/components/inventory/InventoryItemList.tsx
+++ src/components/inventory/InventoryItemList.tsx
@@ -45,7 +45,7 @@ export default function InventoryItemList({ inventoryItemsAfterFilter, catalogIt
 
   if (viewMode === 'grid-view') {
     return (
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
         {renderGroupedInventoryItems(inventoryItemsAfterFilter, catalogItems, viewMode, setSelectedItem)}
       </div>
     );
